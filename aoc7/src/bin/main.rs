022a7a7a3d94a3023a099662554ccde05104e323
Till Rohrmann <trohrmann@apache.org>
2019-03-06 10:48:53 +0100

    [aoc] Finish second part of day 7
    Add work assignment algorithm

--- aoc7/src/bin/main.rs
+++ aoc7/src/bin/main.rs
@@ -1,4 +1,4 @@
-use aoc7::DependencyGraph;
+use aoc7::{DependencyGraph, WorkAssignment, Time};
 
 fn main() {
     let dependencies = aoc7::read_dependencies("input.txt").unwrap();
@@ -10,4 +10,8 @@ fn main() {
     let topological_order = graph.sort_topologically();
 
     println!("Topological sort order: {}", topological_order.iter().collect::<String>());
+
+    let required_time = graph.assign_work(5, 60).last().map(|&WorkAssignment(_, _, Time(_, end_time))| end_time).unwrap_or(0);
+
+    println!("Required time: {}", required_time);
 }
--- aoc7/src/lib.rs
+++ aoc7/src/lib.rs
@@ -6,11 +6,12 @@ use aoc_common::GenericResult;
 use regex::Regex;
 use aoc_common::GenericError;
 use std::rc::Rc;
-use std::collections::{HashSet, BinaryHeap};
+use std::collections::{HashSet, BinaryHeap, VecDeque};
 use std::collections::HashMap;
 use std::cell::RefCell;
 use std::rc::Weak;
 use std::cmp::Ordering;
+use lazy_static::initialize;
 
 #[derive(Debug, PartialEq)]
 pub struct Dependency(char, char);
@@ -53,7 +54,7 @@ pub struct DependencyGraph {
     vertices: HashMap<char, Rc<DependencyVertex>>,
 }
 
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq, Debug)]
 struct Candidate(char);
 
 impl Ord for Candidate {
@@ -67,6 +68,30 @@ impl Ord for Candidate {
 
 impl PartialOrd for Candidate {
     fn partial_cmp(&self, other: &Candidate) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Time(pub usize, pub usize);
+
+#[derive(Debug, PartialEq)]
+pub struct WorkAssignment(pub char, pub usize, pub Time);
+
+#[derive(Eq, PartialEq, Debug)]
+struct OngoingWork(char, usize, Time);
+
+impl Ord for OngoingWork {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let OngoingWork(_, _, Time(_, end_time)) = self;
+        let &OngoingWork(_, _, Time(_, other_end_time)) = other;
+
+        other_end_time.cmp(end_time)
+    }
+}
+
+impl PartialOrd for OngoingWork {
+    fn partial_cmp(&self, other: &OngoingWork) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
@@ -105,6 +130,32 @@ impl DependencyGraph {
         let mut priority_heap: BinaryHeap<Candidate> = BinaryHeap::new();
         let mut result = Vec::with_capacity(self.vertices.len());
 
+        self.initialize_dependencies(&mut unfulfilled_dependencies, &mut priority_heap);
+
+        while let Some(Candidate(next)) = priority_heap.pop() {
+            result.push(next);
+
+            self.complete_requirement(&mut unfulfilled_dependencies, &mut priority_heap, next)
+        }
+
+        result
+    }
+
+    fn complete_requirement(&self, unfulfilled_dependencies: &mut HashMap<char, usize>, priority_heap: &mut BinaryHeap<Candidate>, next: char) {
+        let vertex = self.vertices.get(&next).unwrap();
+        for outgoing in vertex.outgoing.borrow().keys() {
+            if let Some(value) = unfulfilled_dependencies.get_mut(outgoing) {
+                *value -= 1;
+
+                if *value == 0 {
+                    unfulfilled_dependencies.remove(outgoing);
+                    priority_heap.push(Candidate(*outgoing));
+                }
+            }
+        }
+    }
+
+    fn initialize_dependencies(&self, unfulfilled_dependencies: &mut HashMap<char, usize>, priority_heap: &mut BinaryHeap<Candidate>) {
         for dependency_vertex in self.vertices.values() {
             if dependency_vertex.incoming.borrow().len() == 0 {
                 priority_heap.push(Candidate(dependency_vertex.id));
@@ -112,25 +163,45 @@ impl DependencyGraph {
                 unfulfilled_dependencies.insert(dependency_vertex.id, dependency_vertex.incoming.borrow().len());
             }
         }
+    }
 
-        while let Some(Candidate(next)) = priority_heap.pop() {
-            result.push(next);
+    pub fn assign_work(&self, num_workers: usize, constant_work_time: usize) -> Vec<WorkAssignment> {
+        let mut current_time = 0;
+        let mut free_workers = (0..num_workers).collect::<VecDeque<usize>>();
+        let mut work_items: BinaryHeap<Candidate> = BinaryHeap::new();
+        let mut dependencies = HashMap::with_capacity(self.vertices.len());
+        let mut current_work: BinaryHeap<OngoingWork> = BinaryHeap::new();
+        let mut result = Vec::with_capacity(self.vertices.len());
 
-            let vertex = self.vertices.get(&next).unwrap();
+        self.initialize_dependencies(&mut dependencies, &mut work_items);
 
-            for outgoing in vertex.outgoing.borrow().keys() {
-                if let Some(value) = unfulfilled_dependencies.get_mut(outgoing) {
-                    *value -= 1;
-
-                    if *value == 0 {
-                        unfulfilled_dependencies.remove(outgoing);
-                        priority_heap.push(Candidate(*outgoing));
-                    }
+        while !current_work.is_empty() || !work_items.is_empty() {
+            // 1. assign work
+            while let Some(&Candidate(work_item)) = work_items.peek() {
+                if let Some(free_worker) = free_workers.pop_front() {
+                    current_work.push(OngoingWork(work_item, free_worker, Time(current_time, current_time + DependencyGraph::required_time(work_item, constant_work_time))));
+                    work_items.pop();
+                } else {
+                    break;
                 }
+            }
+
+            // 2. advance time to next finished work item
+            if let Some(OngoingWork(finished_work, worker, Time(start, end_time))) = current_work.pop() {
+                current_time = end_time;
+
+                result.push(WorkAssignment(finished_work, worker, Time(start, end_time)));
+                free_workers.push_front(worker);
+
+                self.complete_requirement(&mut dependencies, &mut work_items, finished_work);
             }
         }
 
         result
+    }
+
+    fn required_time(work_item: char, constant_work_time: usize) -> usize {
+        (work_item as usize - 'A' as usize + 1) + constant_work_time
     }
 }
 
@@ -204,5 +275,19 @@ mod tests {
         let dependencies =  vec![Dependency('A', 'B'), Dependency('C', 'B'), Dependency('B', 'D')];
         let graph = DependencyGraph::generate_graph(&dependencies);
         assert_eq!(graph.sort_topologically(), vec!['A', 'C', 'B', 'D']);
+    }
+
+    #[test]
+    fn test_work_assignment() {
+        let dependencies = read_dependencies("test_input.txt").unwrap();
+        let graph = DependencyGraph::generate_graph(&dependencies);
+
+        assert_eq!(graph.assign_work(2, 0), vec![
+            WorkAssignment('C', 0, Time(0, 3)),
+            WorkAssignment('A', 0, Time(3, 4)),
+            WorkAssignment('B', 0, Time(4, 6)),
+            WorkAssignment('F', 1, Time(3, 9)),
+            WorkAssignment('D', 0, Time(6, 10)),
+            WorkAssignment('E', 0, Time(10, 15))])
     }
 }
