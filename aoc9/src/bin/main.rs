ee6a94cbea952dbc2185f321f72cde766f3d2bfa
Till Rohrmann <trohrmann@apache.org>
2019-03-18 08:51:32 +0100

    [aoc] Finish second part of day 9

--- aoc9/src/bin/main.rs
+++ aoc9/src/bin/main.rs
@@ -3,6 +3,9 @@ use aoc9::MarbleGame;
 fn main() {
     let game = MarbleGame::new(458, 72019);
     let result = game.play_game();
-
     println!("{}", result);
+
+    let large_game = MarbleGame::new(458, 72019 * 100);
+    let large_result = large_game.play_game();
+    println!("{}", large_result);
 }
