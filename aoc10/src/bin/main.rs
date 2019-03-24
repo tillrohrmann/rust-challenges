2899b2983a884ba66d79e2a5b9bda063cb02a818
Till Rohrmann <trohrmann@apache.org>
2019-03-24 19:39:18 +0100

    [aoc] Finish part 1 + 2 of day 10

--- aoc10/src/bin/main.rs
+++ aoc10/src/bin/main.rs
@@ -1,15 +1,50 @@
 use aoc_common::read_raw_file_content;
-use aoc10::{parse_points_with_velocity, PointMap};
+use aoc10::{parse_points_with_velocity, PointMap, InitialComponents};
 
 fn main() {
-    let raw_file_content = read_raw_file_content("test_input.txt").unwrap();
+    let raw_file_content = read_raw_file_content("input.txt").unwrap();
     let points_with_velocity = parse_points_with_velocity(&raw_file_content).unwrap();
 
     let mut display_map = PointMap::new(points_with_velocity);
 
-    for i in 0..=4 {
-        println!("Time: {}", i);
-        display_map.display();
+    println!("Size: {:?}", display_map.size());
+
+    let number_points = display_map.number_points();
+    let mut counter = 0;
+    let mut min_connected_components = usize::max_value();
+    let mut min_connected_components_counter = 0;
+
+    while display_map.number_points() == number_points {
+        let connected_components = InitialComponents::new(
+            display_map.points_iter().collect(),
+            display_map.size()).calculate_connected_components();
+
+        if min_connected_components > connected_components.number_connected_components() {
+            min_connected_components = connected_components.number_connected_components();
+            min_connected_components_counter = counter;
+        }
+
+        if connected_components.number_connected_components() < 100 {
+            display_map.display();
+            println!();
+            println!();
+        }
+
         display_map.advance();
+        counter += 1;
     }
+
+    println!("{}", counter);
+    println!("Min CCs: {}, counter: {}", min_connected_components, min_connected_components_counter);
+
+//    for i in 0..=1000 {
+//        println!("Time: {}", i);
+//        let connected_components = InitialComponents::new(
+//            display_map.points_iter().collect(),
+//            display_map.size()).calculate_connected_components();
+//
+//        println!("CC: {}", connected_components.number_connected_components());
+//
+//        display_map.advance();
+//    }
 }
--- aoc10/src/lib.rs
+++ aoc10/src/lib.rs
@@ -1,7 +1,9 @@
 #[macro_use] extern crate lazy_static;
 use aoc_common::{GenericResult, GenericError};
 use regex::Regex;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
+use std::collections::vec_deque::VecDeque;
+use std::cmp::Ordering;
 
 #[derive(Debug, PartialOrd, PartialEq, Copy, Clone, Eq, Hash)]
 pub struct Vector {
@@ -48,7 +50,7 @@ impl std::ops::AddAssign for Vector {
     }
 }
 
-#[derive(Debug, PartialOrd, PartialEq)]
+#[derive(Debug, PartialOrd, PartialEq, Clone)]
 pub struct PointWithVelocity {
     position: Vector,
     velocity: Vector,
@@ -98,12 +100,14 @@ pub struct PointMap {
 }
 
 impl PointMap {
-    const EMPTY_CHAR: char = ' ';
+    const EMPTY_CHAR: char = '.';
     const POINT_CHAR: char = '#';
+    const ORIGIN: Vector = Vector{ x: 0, y: 0};
 
     pub fn new(points_with_velocity: Vec<PointWithVelocity>) -> PointMap {
-        let (min_x, max_x) = PointMap::find_min_max(&points_with_velocity, |pv| pv.position.x);
-        let (min_y, max_y) = PointMap::find_min_max(&points_with_velocity, |pv| pv.position.y);
+        let points = points_with_velocity.iter().map(|p| p.position).collect();
+        let (min_x, max_x) = PointMap::find_min_max(&points, |p| p.x);
+        let (min_y, max_y) = PointMap::find_min_max(&points, |p| p.y);
         let min_vector = Vector::new(min_x, min_y);
         let size = Vector::new(max_x - min_x + 1, max_y - min_y + 1);
 
@@ -120,7 +124,7 @@ impl PointMap {
         }
     }
 
-    fn find_min_max(points_with_velocity: &Vec<PointWithVelocity>, field_selector: fn(&PointWithVelocity) -> isize) -> (isize, isize) {
+    fn find_min_max(points_with_velocity: &Vec<Vector>, field_selector: fn(&Vector) -> isize) -> (isize, isize) {
         let min = points_with_velocity.iter().map(field_selector).min().unwrap_or(0);
         let max = points_with_velocity.iter().map(field_selector).max().unwrap_or(0);
 
@@ -128,13 +132,45 @@ impl PointMap {
     }
 
     pub fn display(&self) {
-        let mut field = vec![vec![PointMap::EMPTY_CHAR; self.size.x as usize]; self.size.y as usize];
-        for point in self.points_with_velocity.iter() {
-            field[point.position.y as usize][point.position.x as usize] = PointMap::POINT_CHAR;
-        }
+        let mut points: Vec<Vector> = self.points_iter().collect::<HashSet<Vector>>().into_iter().collect();
 
-        for line in field.iter() {
-            println!("{:?}", line);
+        points.sort_by(|a, b| {
+            if a.y == b.y {
+                a.x.cmp(&b.x)
+            } else {
+                a.y.cmp(&b.y)
+            }
+        });
+
+        dbg!(&points);
+
+        let (min_x, max_x) = PointMap::find_min_max(&points, |p| p.x);
+        let (min_y, max_y) = PointMap::find_min_max(&points, |p| p.y);
+
+        let size = dbg!(Vector::new(max_x - min_x + 1, max_y - min_y + 1));
+        let origin = dbg!(Vector::new(min_x, min_y));
+
+        let mut current_position = Vector::new(0, 0);
+
+        for point in points.iter() {
+            let normalized_point = *point - origin;
+            while current_position.y < normalized_point.y {
+                for _ in current_position.x..size.x {
+                    print!("{}", PointMap::EMPTY_CHAR);
+                }
+                println!();
+                current_position.y += 1;
+                current_position.x = 0;
+                current_position;
+            }
+
+            while current_position.x < normalized_point.x {
+                print!("{}", PointMap::EMPTY_CHAR);
+                current_position.x += 1;
+            }
+
+            print!("{}", PointMap::POINT_CHAR);
+            current_position.x += 1;
         }
     }
 
@@ -142,6 +178,113 @@ impl PointMap {
         for point in self.points_with_velocity.iter_mut() {
             point.position += point.velocity;
         }
+
+        // filter out points which have left the valid map
+        self.points_with_velocity = self.points_with_velocity.iter()
+            .filter(|point_with_velocity| {
+                let position = point_with_velocity.position;
+                PointMap::ORIGIN <= position && position < self.size
+            })
+            .map(|p| p.clone())
+            .collect();
+    }
+
+    pub fn points_iter<'a>(&'a self) -> impl Iterator<Item = Vector> + 'a {
+        self.points_with_velocity.iter().map(|point_with_velocity| point_with_velocity.position)
+    }
+
+    pub fn number_points(&self) -> usize {
+        self.points_with_velocity.len()
+    }
+
+    pub fn size(&self) -> Vector {
+        self.size
+    }
+}
+
+pub struct InitialComponents {
+    components: Vec<Vector>,
+    edges: HashMap<Vector, Vec<Vector>>,
+    size: Vector,
+}
+
+pub struct ConnectedComponents {
+    connected_components: HashMap<usize, usize>,
+}
+
+impl InitialComponents {
+    pub fn new(components: Vec<Vector>, size: Vector) -> InitialComponents {
+        let mut edges : HashMap<Vector, Vec<Vector>>= components.iter().cloned().map(|vector| (vector, vec![])).collect();
+
+        for point in components.iter() {
+            InitialComponents::check_edge_exists(&mut edges, point, Vector::new(point.x + 1, point.y));
+            InitialComponents::check_edge_exists(&mut edges, point, Vector::new(point.x - 1, point.y));
+            InitialComponents::check_edge_exists(&mut edges, point, Vector::new(point.x, point.y + 1));
+            InitialComponents::check_edge_exists(&mut edges, point, Vector::new(point.x, point.y - 1));
+        }
+
+        InitialComponents {
+            components,
+            edges,
+            size,
+        }
+    }
+
+    fn check_edge_exists(edges: &mut HashMap<Vector, Vec<Vector>>, point: &Vector, xp: Vector) -> () {
+        if edges.contains_key(&xp) {
+            edges.entry(*point).and_modify(|edges| edges.push(xp));
+        }
+    }
+
+    pub fn calculate_connected_components(mut self) -> ConnectedComponents {
+        let mut queue = self.components.iter().cloned().collect::<VecDeque<Vector>>();
+        let mut map: HashMap<Vector, usize> = self.components.iter().enumerate().map(|(index, point)| (point.clone(), index)).collect();
+
+        while let Some(point) = queue.pop_front() {
+            let neighbours = self.edges.get(&point).unwrap();
+
+            let min_neighbour = InitialComponents::find_min_neighbour(&map, neighbours);
+
+            if let Some(neighbour) = min_neighbour {
+                map.entry(point).and_modify(|value| {
+                    if neighbour < *value {
+                        *value = neighbour;
+
+                        for neighbour in neighbours.iter() {
+                            queue.push_back(neighbour.clone());
+                        }
+                    }
+                });
+            }
+        }
+
+        let mut connected_components : HashMap<usize, usize>= HashMap::new();
+
+        for &connected_component in map.values() {
+            *connected_components.entry(connected_component).or_insert(0) += 1;
+        }
+
+        ConnectedComponents::new(connected_components)
+    }
+
+    fn find_min_neighbour(map: &HashMap<Vector, usize>, neighbours: &Vec<Vector>) -> Option<usize> {
+        neighbours
+            .iter()
+            .map(|neighbour| map.get(neighbour).cloned())
+            .flatten()
+            .min()
+    }
+}
+
+impl ConnectedComponents {
+    pub fn new(connected_components: HashMap<usize, usize>) -> ConnectedComponents {
+        ConnectedComponents {
+            connected_components,
+        }
+    }
+
+    pub fn number_connected_components(&self) -> usize {
+        self.connected_components.len()
     }
 }
 
