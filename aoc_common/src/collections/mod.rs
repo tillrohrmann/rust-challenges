a959a6087c59cbcd91d0bfa4f507ac9a5620aff1
Till Rohrmann <trohrmann@apache.org>
2019-03-26 08:40:22 +0100

    [common] Start RadixTree implementation

--- aoc_common/src/collections/mod.rs
+++ aoc_common/src/collections/mod.rs
@@ -1,3 +1,5 @@
 pub mod circular_linked_list;
+pub mod radix_tree;
 
 pub use self::circular_linked_list::CircularLinkedList;
+pub use self::radix_tree::RadixTree;
---
+++ aoc_common/src/collections/radix_tree.rs
@@ -0,0 +1,9 @@
+pub struct RadixTree {
+
+}
+
+impl RadixTree {
+    pub fn contains(&self, key: &str) -> bool {
+        unimplemented!()
+    }
+}
