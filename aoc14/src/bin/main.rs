d65fde2c9ebbbda4678907204f419d0145505b85
Till Rohrmann <trohrmann@apache.org>
2019-04-01 00:37:11 +0200

    [aoc] Solve second part of day 14

--- aoc14/src/bin/main.rs
+++ aoc14/src/bin/main.rs
@@ -1,7 +1,16 @@
 use aoc14::Recipes;
 
 fn main() {
-    let mut recipes = Recipes::new();
+//    solve_part_one();
+    solve_part_two();
+}
 
+fn solve_part_one() {
+    let mut recipes = Recipes::new();
     println!("{:?}", recipes.find_recipes_after(236021, 10));
+}
+
+fn solve_part_two() {
+    let mut recipes = Recipes::new();
+    println!("{}", recipes.number_recipes_until_sequence(&[2,3,6,0,2,1]));
 }
--- aoc14/src/lib.rs
+++ aoc14/src/lib.rs
@@ -1,4 +1,5 @@
 use std::collections::vec_deque::VecDeque;
+use std::collections::HashSet;
 
 pub struct Recipes {
     recipes: Vec<usize>,
@@ -17,30 +18,73 @@ impl Recipes {
 
     pub fn find_recipes_after(&mut self, warmup_count: usize, recipe_count: usize) -> &[usize] {
         while self.recipes.len() < warmup_count + recipe_count {
-            let mut current_sum: usize = self.elves.iter().map(|&i| self.recipes[i]).sum();
-
-            let mut digits = VecDeque::with_capacity(2);
-
-            if current_sum == 0 {
-                self.recipes.push(0);
-            } else {
-                while current_sum > 0 {
-                    digits.push_back(current_sum % 10);
-                    current_sum = current_sum / 10;
-                }
-
-                while let Some(digit) = digits.pop_back() {
-                    self.recipes.push(digit);
-                }
-            }
-
-            self.elves = self.elves.iter().map(|&i| (1 + self.recipes[i] + i) % self.recipes.len()).collect();
-
+            self.next_round();
         }
 
         &self.recipes[warmup_count..(warmup_count + recipe_count)]
     }
+
+    fn next_round(&mut self) -> Vec<usize> {
+        let mut current_sum: usize = self.elves.iter().map(|&i| self.recipes[i]).sum();
+        let mut digits = VecDeque::with_capacity(2);
+
+        let result = if current_sum == 0 {
+            self.recipes.push(0);
+            vec![0]
+        } else {
+            while current_sum > 0 {
+                digits.push_back(current_sum % 10);
+                current_sum = current_sum / 10;
+            }
+
+            let mut result = Vec::with_capacity(digits.len());
+            while let Some(digit) = digits.pop_back() {
+                self.recipes.push(digit);
+                result.push(digit);
+            }
+
+            result
+        };
+
+        self.elves = self.elves.iter().map(|&i| (1 + self.recipes[i] + i) % self.recipes.len()).collect();
+
+        result
+    }
+
+    pub fn number_recipes_until_sequence(&mut self, sequence: &[usize]) -> usize {
+        let mut candidates = HashSet::new();
+        while candidates.iter().filter(|Candidate(index, length)| *length == sequence.len()).count() == 0 {
+            let length = self.recipes.len();
+            let next_digits = self.next_round();
+
+            candidates = candidates.into_iter().map(|Candidate(index, length)| {
+                let suffix_length = std::cmp::min(next_digits.len(), sequence.len() - length);
+                if sequence[length..(length + suffix_length)] == next_digits[0..suffix_length] {
+                    Some(Candidate(index, length + suffix_length))
+                } else {
+                    None
+                }
+            }).flatten().collect();
+
+            for i in 0..next_digits.len() {
+                if next_digits[i..] == sequence[0..(next_digits.len() - i)] {
+                    candidates.insert(Candidate(length + i, next_digits.len() - i));
+                }
+            }
+        }
+
+        let Candidate(index, length) = dbg!(candidates)
+            .into_iter()
+            .filter(|Candidate(index, length)| *length == sequence.len())
+            .min_by(|Candidate(a, _), Candidate(b, _)| a.cmp(b))
+            .unwrap();
+
+        index
+    }
 }
+
+#[derive(Hash, Eq, PartialEq, Debug)]
+struct Candidate(usize, usize);
 
 #[cfg(test)]
 mod tests {
@@ -68,5 +112,29 @@ mod tests {
     fn test_recipe_calculation_4() {
         let mut recipes = Recipes::new();
         assert_eq!(recipes.find_recipes_after(2018, 10), &[5,9,4,1,4,2,9,8,8,2])
+    }
+
+    #[test]
+    fn test_recipes_until_sequence_1() {
+        let mut recipes = Recipes::new();
+        assert_eq!(recipes.number_recipes_until_sequence(&[5,1,5,8,9]), 9);
+    }
+
+    #[test]
+    fn test_recipes_until_sequence_2() {
+        let mut recipes = Recipes::new();
+        assert_eq!(recipes.number_recipes_until_sequence(&[0,1,2,4,5]), 5);
+    }
+
+    #[test]
+    fn test_recipes_until_sequence_3() {
+        let mut recipes = Recipes::new();
+        assert_eq!(recipes.number_recipes_until_sequence(&[9,2,5,1,0]), 18);
+    }
+
+    #[test]
+    fn test_recipes_until_sequence_4() {
+        let mut recipes = Recipes::new();
+        assert_eq!(recipes.number_recipes_until_sequence(&[5,9,4,1,4]), 2018);
     }
 }
