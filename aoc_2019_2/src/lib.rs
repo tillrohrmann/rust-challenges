2f03c4b61f02e1872ae21a1eaee7f6a5040bde76
Till Rohrmann <trohrmann@apache.org>
2019-12-30 13:20:55 +0100

    [aoc_2019] Solution for the first part of day 5

--- aoc_2019_2/src/lib.rs
+++ aoc_2019_2/src/lib.rs
@@ -1,8 +1,9 @@
 use crate::ComputationResult::{Success, Failure};
-use crate::Command::{Stop, Add, Multiply};
+use crate::Command::{*};
+use std::io;
 
 pub struct IntComputer {
-    memory: Vec<u32>,
+    memory: Vec<i32>,
     pointer: usize,
 }
 
@@ -13,9 +14,26 @@ pub enum ComputationResult {
 }
 
 enum Command {
-    Add(usize, usize, usize),
-    Multiply(usize, usize, usize),
+    Add(InputParameter, InputParameter, usize),
+    Multiply(InputParameter, InputParameter, usize),
+    Input(usize),
+    Output(InputParameter),
     Stop,
+}
+
+#[derive(Copy, Clone)]
+enum InputParameter {
+    Value(i32),
+    Position(usize),
+}
+
+impl InputParameter {
+    fn interpret(&self, memory: &mut Vec<i32>) -> i32 {
+        match *self {
+            InputParameter::Value(value) => value,
+            InputParameter::Position(pos) => memory[pos],
+        }
+    }
 }
 
 impl CommandLike for Command {
@@ -31,14 +49,45 @@ impl CommandLike for Command {
             Stop => 1,
             Multiply(_, _, _) => 4,
             Add(_, _, _) => 4,
+            Input(_) => 2,
+            Output(_) => 2,
         }
     }
 
-    fn execute(&self, memory: &mut Vec<u32>) {
+    fn execute(&self, memory: &mut Vec<i32>) {
         match *self {
             Stop => (),
-            Add(src_a, src_b, dst) => memory[dst] = memory[src_a] + memory[src_b],
-            Multiply(src_a, src_b, dst) => memory[dst] = memory[src_a] * memory[src_b],
+            Add(parameter_a, parameter_b, dst) => {
+                let value_a = parameter_a.interpret(memory);
+                let value_b = parameter_b.interpret(memory);
+
+                memory[dst] = value_a + value_b;
+            },
+            Multiply(parameter_a, parameter_b, dst) => {
+                let value_a = parameter_a.interpret(memory);
+                let value_b = parameter_b.interpret(memory);
+                memory[dst] = value_a * value_b;
+            },
+            Input(dst) => {
+                loop {
+                    println!("Request user input:");
+                    let mut line = String::new();
+                    io::stdin().read_line(&mut line);
+
+                    let parsed_result = line.trim().parse();
+
+                    if let Ok(value) = parsed_result {
+                        memory[dst] = value;
+                        break;
+                    } else {
+                        println!("Could not parse user input. Please type again.")
+                    }
+                }
+            },
+            Output(parameter) => {
+                let value = parameter.interpret(memory);
+                println!("Output value: {}", value);
+            }
         }
     }
 }
@@ -48,11 +97,11 @@ trait CommandLike {
 
     fn command_length(&self) -> usize;
 
-    fn execute(&self, memory: &mut Vec<u32>);
+    fn execute(&self, memory: &mut Vec<i32>);
 }
 
 impl IntComputer {
-    pub fn new(memory: Vec<u32>) -> IntComputer {
+    pub fn new(memory: Vec<i32>) -> IntComputer {
         IntComputer { memory, pointer: 0 }
     }
 
@@ -77,24 +126,59 @@ impl IntComputer {
         }
     }
 
-    pub fn memory(&self) -> &Vec<u32> {
+    pub fn memory(&self) -> &Vec<i32> {
         &self.memory
     }
 
     fn next_command(&mut self) -> Result<Command, String> {
         let view = &self.memory[self.pointer as usize..];
-        match view[0] {
+        let opcode_with_modes = view[0];
+        let parameters = &view[1..];
+        let opcode = opcode_with_modes % 100;
+        let modes = opcode_with_modes / 100;
+        match opcode {
             1 => {
-                Ok(Add(view[1] as usize, view[2] as usize, view[3] as usize))
+                let input_parameters = IntComputer::parse_input_parameters(parameters, modes, 2);
+                Ok(Add(input_parameters[0], input_parameters[1], parameters[2] as usize))
             },
             2 => {
-                Ok(Multiply(view[1] as usize, view[2] as usize, view[3] as usize))
+                let input_parameters = IntComputer::parse_input_parameters(parameters, modes, 2);
+                Ok(Multiply(input_parameters[0], input_parameters[1], parameters[2] as usize))
             },
+            3 => {
+                Ok(Input(parameters[0] as usize))
+            },
+            4 => {
+                let input_parameters = IntComputer::parse_input_parameters(parameters, modes, 1);
+                Ok(Output(input_parameters[0]))
+            }
             99 => {
                 Ok(Stop)
             },
             x => Err(format!("Could not parse command {}.", x))
         }
+    }
+
+    fn parse_input_parameters(memory: &[i32], modes: i32, number_parameters: u32) -> Vec<InputParameter> {
+        let mut result = Vec::new();
+        let mut modes = modes;
+
+        for idx in 0 as usize..number_parameters as usize {
+            let mode = modes % 10;
+
+            let parameter = if mode == 0 {
+                InputParameter::Position(memory[idx] as usize)
+            } else if mode == 1 {
+                InputParameter::Value(memory[idx])
+            } else {
+                panic!("Unknown mode value: {}", mode);
+            };
+
+            result.push(parameter);
+            modes /= 10;
+        }
+
+        result
     }
 }
 
@@ -130,7 +214,7 @@ mod tests {
         run_test(input, &output);
     }
 
-    fn run_test(input: Vec<u32>, output: &Vec<u32>) {
+    fn run_test(input: Vec<i32>, output: &Vec<i32>) {
         let mut computer = IntComputer::new(input);
         assert_eq!(computer.compute(), ComputationResult::Success);
         assert_eq!(computer.memory(), output);
--- aoc_2019_2/src/main.rs
+++ aoc_2019_2/src/main.rs
@@ -4,24 +4,42 @@ use std::str::FromStr;
 use aoc_2019_2::ComputationResult::Success;
 
 fn main() {
-    let input = fs::read_to_string("input.txt").unwrap();
-    let memory: Vec<u32> = input
-        .trim()
-        .split(',')
-        .map(|split| u32::from_str(split).unwrap())
-        .collect();
-    compute_result(&memory);
+    solve_day_5();
+}
+
+fn solve_day_5() {
+    let memory = read_memory_from_file("input_day5.txt");
+    compute_result(memory);
+}
+
+fn solve_day_2() {
+    let memory = read_memory_from_file("input.txt");
+    compute_result_with_modified_memory(&memory);
     find_verb_noun_for(&memory, 19690720);
 }
 
-fn compute_result(memory: &Vec<u32>) {
+fn read_memory_from_file(path: &str) -> Vec<i32> {
+    let input = fs::read_to_string(path).unwrap();
+    let memory: Vec<i32> = input
+        .trim()
+        .split(',')
+        .map(|split| i32::from_str(split).unwrap())
+        .collect();
+    memory
+}
+
+fn compute_result_with_modified_memory(memory: &Vec<i32>) {
     let derived_memory = create_modified_memory(&memory, 12, 2);
-    let mut computer = IntComputer::new(derived_memory);
+    compute_result(derived_memory);
+}
+
+fn compute_result(memory: Vec<i32>) {
+    let mut computer = IntComputer::new(memory);
     assert_eq!(computer.compute(), ComputationResult::Success);
     println!("{:?}", computer.memory()[0]);
 }
 
-fn create_modified_memory(original_input: &Vec<u32>, noun: u32, verb: u32) -> Vec<u32> {
+fn create_modified_memory(original_input: &Vec<i32>, noun: i32, verb: i32) -> Vec<i32> {
     let mut result = original_input.clone();
     result[1] = noun;
     result[2] = verb;
@@ -29,7 +47,7 @@ fn create_modified_memory(original_input: &Vec<u32>, noun: u32, verb: u32) -> Ve
     result
 }
 
-fn find_verb_noun_for(original_memory_input: &Vec<u32>, target_value: u32) {
+fn find_verb_noun_for(original_memory_input: &Vec<i32>, target_value: i32) {
     for noun in 0..=99 {
         for verb in 0..=99 {
             let modified_memory = create_modified_memory(original_memory_input, noun, verb);
