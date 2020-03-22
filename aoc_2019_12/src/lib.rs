161a63194c3c6e4b511db9ab97d53341aedfc0b6
Till Rohrmann <trohrmann@apache.org>
2020-03-22 17:17:21 +0100

    [aoc] Solve second part of day 12

--- aoc_2019_12/src/lib.rs
+++ aoc_2019_12/src/lib.rs
@@ -1,7 +1,7 @@
-use aoc_common::math::Point3d;
+use aoc_common::math::{Point3d, IntErrorKind};
 use std::cmp::Ordering;
 
-#[derive(Debug, PartialOrd, PartialEq)]
+#[derive(Debug, PartialOrd, PartialEq, Clone, Copy)]
 pub struct Moon {
     position: Point3d,
     velocity: Point3d,
@@ -21,7 +21,7 @@ impl Moon {
         let Point3d(r, s, t) = self.velocity;
 
         let potential = Moon::calculate_energy(vec![x, y, z]);
-        let kinetic = Moon::calculate_energy(vec![r, s ,t]);
+        let kinetic = Moon::calculate_energy(vec![r, s, t]);
 
         kinetic * potential
     }
@@ -57,6 +57,10 @@ impl MoonSimulator {
         for _ in 0..steps {
             self.simulate_step();
         }
+    }
+
+    pub fn moons(&self) -> &Vec<Moon> {
+        &self.moons
     }
 
     fn simulate_step(&mut self) {
@@ -97,6 +101,92 @@ impl MoonSimulator {
             Ordering::Equal => 0,
         }
     }
+
+    pub fn find_period(&self) -> Result<usize, IntErrorKind> {
+        let moons_x = self.moons.iter().map(|&moon| Moon1d::new_x(moon)).collect();
+        let moons_y = self.moons.iter().map(|&moon| Moon1d::new_y(moon)).collect();
+        let moons_z = self.moons.iter().map(|&moon| Moon1d::new_z(moon)).collect();
+
+        let x_period = PeriodFinder::find_period(moons_x);
+        let y_period = PeriodFinder::find_period(moons_y);
+        let z_period = PeriodFinder::find_period(moons_z);
+
+        aoc_common::math::least_common_multiple(&vec![x_period, y_period, z_period])
+    }
+}
+
+#[derive(Copy, Clone, PartialEq)]
+struct Moon1d {
+    position: isize,
+    velocity: isize,
+}
+
+impl Moon1d {
+    fn new_x(moon: Moon) -> Moon1d {
+        Moon1d {
+            position: moon.position.0,
+            velocity: moon.velocity.0,
+        }
+    }
+
+    fn new_y(moon: Moon) -> Moon1d {
+        Moon1d {
+            position: moon.position.1,
+            velocity: moon.velocity.1,
+        }
+    }
+
+    fn new_z(moon: Moon) -> Moon1d {
+        Moon1d {
+            position: moon.position.2,
+            velocity: moon.velocity.2,
+        }
+    }
+}
+
+pub struct PeriodFinder {}
+
+impl PeriodFinder {
+    fn find_period(mut initial_configuration: Vec<Moon1d>) -> usize {
+       let mut moons = initial_configuration.clone();
+
+        let mut period = 0;
+
+        loop {
+            period += 1;
+
+            moons = PeriodFinder::simulate_step(moons);
+
+            if moons == initial_configuration {
+                break;
+            }
+        }
+
+        period
+    }
+
+    fn simulate_step(mut moons: Vec<Moon1d>) -> Vec<Moon1d> {
+        PeriodFinder::update_velocity(&mut moons);
+        PeriodFinder::update_position(&mut moons);
+
+        moons
+    }
+
+    fn update_velocity(moons: &mut Vec<Moon1d>) {
+        for i in 0..moons.len() {
+            for j in i+1..moons.len() {
+                let gravity = MoonSimulator::gravity(moons[i].position, moons[j].position);
+                moons[i].velocity += gravity;
+                moons[j].velocity -= gravity;
+            }
+        }
+    }
+
+    fn update_position(moons: &mut Vec<Moon1d>) {
+        for moon in moons.iter_mut() {
+            moon.position += moon.velocity
+        }
+    }
 }
 
 #[cfg(test)]
@@ -116,6 +206,20 @@ mod tests {
             Point3d(2, -10, -7),
             Point3d(4, -8, 8),
             Point3d(3, 5, -1),
+        ];
+
+        positions
+            .into_iter()
+            .map(|position| Moon::new_static_moon(position))
+            .collect()
+    }
+
+    fn create_moons_example_two() -> Vec<Moon> {
+        let positions = vec![
+            Point3d(-8, -10, 0),
+            Point3d(5, 5, 10),
+            Point3d(2, -7, 3),
+            Point3d(9, -8, -3),
         ];
 
         positions
@@ -156,5 +260,17 @@ mod tests {
     ) -> () {
         simulator.simulate_steps(i);
         assert_eq!(simulator.moons, expected_moon_states)
+    }
+
+    #[test]
+    fn find_period_example_one() {
+        let mut simulator = MoonSimulator::new(create_moons_example_one());
+        assert_eq!(simulator.find_period().unwrap(), 2772);
+    }
+
+    #[test]
+    fn find_period_example_two() {
+        let mut simulator = MoonSimulator::new(create_moons_example_two());
+        assert_eq!(simulator.find_period().unwrap(), 4686774924);
     }
 }
--- aoc_2019_12/src/main.rs
+++ aoc_2019_12/src/main.rs
@@ -2,6 +2,39 @@ use aoc_2019_12::{Moon, MoonSimulator};
 use aoc_common::math::Point3d;
 
 fn main() {
+    solve_day_12_2()
+}
+
+fn create_simple_moons() -> Vec<Moon> {
+    let starting_positions = vec![
+        Point3d(-1, 0, 0),
+        Point3d(2, 0, 0),
+        Point3d(4, 0, 0),
+        Point3d(3, 0, 0),
+    ];
+
+    let moons = starting_positions
+        .into_iter()
+        .map(|point| Moon::new_static_moon(point))
+        .collect();
+
+    moons
+}
+
+fn solve_day_12_1() -> () {
+    let moons = create_moons();
+    let mut simulator = MoonSimulator::new(moons);
+    simulator.simulate_steps(1000);
+    println!("Energy: {}", simulator.energy())
+}
+
+fn solve_day_12_2() {
+    let moons = create_moons();
+    let mut simulator = MoonSimulator::new(moons);
+    println!("Period: {}", simulator.find_period().unwrap());
+}
+
+fn create_moons() -> Vec<Moon> {
     let starting_positions = vec![
         Point3d(17, 5, 1),
         Point3d(-2, -8, 8),
@@ -12,8 +45,5 @@ fn main() {
         .into_iter()
         .map(|point| Moon::new_static_moon(point))
         .collect();
-    let mut simulator = MoonSimulator::new(moons);
-
-    simulator.simulate_steps(1000);
-    println!("Energy: {}", simulator.energy())
+    moons
 }
