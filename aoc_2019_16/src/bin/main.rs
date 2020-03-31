7ae7ef5abce26b7ff2565bee78c3fe2c4730e7dd
Till Rohrmann <trohrmann@apache.org>
2020-03-31 08:59:00 +0200

    [aoc_2019] Add tests for day 16 part 1

--- aoc_2019_16/src/bin/main.rs
+++ aoc_2019_16/src/bin/main.rs
@@ -1,22 +1,12 @@
 use std::fs;
 
 fn main() {
-    const RADIX: u32 = 10;
     let content = fs::read_to_string("input.txt").unwrap();
-    let input: Result<Vec<isize>, String> = content
-        .trim()
-        .chars()
-        .map(|chr| {
-            chr.to_digit(RADIX)
-                .map(|i| i as isize)
-                .ok_or(format!("Could not parse char {} as digit.", chr))
-        })
-        .collect();
+    let digits = aoc_2019_16::split_string_into_digits(content).unwrap();
 
-    let input = input.unwrap();
     let fft = aoc_2019_16::FFT::new();
 
-    let output = fft.calculate(&input);
+    let output = fft.calculate(&digits, 100);
 
     println!("{:?}", output);
 }
--- aoc_2019_16/src/lib.rs
+++ aoc_2019_16/src/lib.rs
@@ -7,12 +7,54 @@ impl FFT {
         FFT {}
     }
 
-    pub fn calculate(&self, input: &Vec<isize>) -> Vec<isize> {
+    pub fn calculate(&self, input: &Vec<isize>, num_phases: usize) -> Vec<isize> {
         vec![]
     }
 }
 
+pub fn split_string_into_digits(content: String) -> Result<Vec<isize>, String> {
+    const RADIX: u32 = 10;
+    content
+        .trim()
+        .chars()
+        .map(|chr| {
+            chr.to_digit(RADIX)
+                .map(|i| i as isize)
+                .ok_or(format!("Could not parse char {} as digit.", chr))
+        })
+        .collect()
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn example_one() {
+        let input = "80871224585914546619083218645595";
+        let expected_output = "24176176";
+        run_fft_test(input, expected_output);
+    }
+
+    #[test]
+    fn example_two() {
+        let input = "19617804207202209144916044189917";
+        let expected_output = "73745418";
+        run_fft_test(input, expected_output);
+    }
+
+    #[test]
+    fn example_three() {
+        let input = "69317163492948606335995924319873";
+        let expected_output = "52432133";
+        run_fft_test(input, expected_output);
+    }
+
+    fn run_fft_test(input: &str, expected_output: &str) {
+        let input = split_string_into_digits(input.into()).unwrap();
+        let fft = FFT::new();
+        let output = fft.calculate(&input, 100);
+        assert_eq!(output.iter().take(8).map(|&d| char::from(d as u8)).collect::<String>(), expected_output);
+    }
 }
