91cb7db1c5429a3c5d24256d8824c52a865eef38
Till Rohrmann <trohrmann@apache.org>
2020-04-01 09:59:14 +0200

    [aoc_2019] Solve day 16 part 1

--- aoc_2019_16/src/bin/main.rs
+++ aoc_2019_16/src/bin/main.rs
@@ -4,9 +4,9 @@ fn main() {
     let content = fs::read_to_string("input.txt").unwrap();
     let digits = aoc_2019_16::split_string_into_digits(content).unwrap();
 
-    let fft = aoc_2019_16::FFT::new();
+    let fft = aoc_2019_16::FFT::new(&digits);
 
-    let output = fft.calculate(&digits, 100);
+    let output = fft.calculate(100);
 
     println!("{:?}", output);
 }
--- aoc_2019_16/src/lib.rs
+++ aoc_2019_16/src/lib.rs
@@ -1,14 +1,64 @@
-pub struct FFT {
-
+pub struct FFT<'a> {
+    input: &'a Vec<isize>,
+    frequencies: Vec<Vec<isize>>,
 }
 
-impl FFT {
-    pub fn new() -> FFT {
-        FFT {}
+impl<'a> FFT<'a> {
+    const BASE_FREQUENCY: [isize; 4] = [0, 1, 0, -1];
+
+    pub fn new(input: &'a Vec<isize>) -> FFT {
+        let frequencies = FFT::calculate_frequencies(input.len());
+
+        FFT { input, frequencies }
     }
 
-    pub fn calculate(&self, input: &Vec<isize>, num_phases: usize) -> Vec<isize> {
-        vec![]
+    fn calculate_frequencies(number_frequencies: usize) -> Vec<Vec<isize>> {
+        (0..number_frequencies)
+            .map(|frequency| FFT::calculate_frequency(frequency, number_frequencies))
+            .collect()
+    }
+
+    fn calculate_frequency(frequency: usize, length: usize) -> Vec<isize> {
+        (0..length)
+            .map(|idx| FFT::calculate_frequency_entry(idx, frequency))
+            .collect()
+    }
+
+    fn calculate_frequency_entry(idx: usize, frequency: usize) -> isize {
+        let period = FFT::BASE_FREQUENCY.len() * (frequency + 1);
+        let period_idx = (idx + 1) % period;
+        let base_period_idx = period_idx / (frequency + 1);
+
+        FFT::BASE_FREQUENCY[base_period_idx]
+    }
+
+    pub fn calculate(&self, num_phases: usize) -> Vec<isize> {
+        let length = self.input.len();
+        let mut result = self.input.clone();
+
+        for _ in 0..num_phases {
+            let new_result = (0..length)
+                .map(|idx| self.calculate_entry(&result, idx))
+                .collect();
+
+            result = new_result;
+        }
+
+        result
+    }
+
+    fn calculate_entry(&self, input: &Vec<isize>, idx: usize) -> isize {
+        let result: isize = input
+            .iter()
+            .zip(self.get_frequency(idx))
+            .map(|(a, b)| a * b)
+            .sum();
+
+        isize::abs(result % 10)
+    }
+
+    fn get_frequency(&self, idx: usize) -> &Vec<isize> {
+        self.frequencies.get(idx).unwrap()
     }
 }
 
@@ -25,10 +75,35 @@ pub fn split_string_into_digits(content: String) -> Result<Vec<isize>, String> {
         .collect()
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn frequency_calculation_one() {
+        let frequency = FFT::calculate_frequency(0, 8);
+
+        assert_eq!(frequency, vec![1, 0, -1, 0, 1, 0, -1, 0]);
+    }
+
+    #[test]
+    fn frequency_calculation_two() {
+        let frequency = FFT::calculate_frequency(1, 8);
+
+        assert_eq!(frequency, vec![0, 1, 1, 0, 0, -1, -1, 0]);
+    }
+
+    #[test]
+    fn frequency_calculation_three() {
+        let frequency = FFT::calculate_frequency(2, 8);
+
+        assert_eq!(frequency, vec![0, 0, 1, 1, 1, 0, 0, 0]);
+    }
+
+    #[test]
+    fn modulo_negative() {
+        assert_eq!(4, isize::abs(-14 % 10));
+    }
 
     #[test]
     fn example_one() {
@@ -53,8 +128,17 @@ mod tests {
 
     fn run_fft_test(input: &str, expected_output: &str) {
         let input = split_string_into_digits(input.into()).unwrap();
-        let fft = FFT::new();
-        let output = fft.calculate(&input, 100);
-        assert_eq!(output.iter().take(8).map(|&d| char::from(d as u8)).collect::<String>(), expected_output);
+        let fft = FFT::new(&input);
+        let output = fft.calculate(100);
+        assert_eq!(
+            output
+                .iter()
+                .take(8)
+                .map(|&d| {
+                   d.to_string()
+                })
+                .collect::<String>(),
+            expected_output
+        );
     }
 }
