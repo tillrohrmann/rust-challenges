1e72f0252dc519dafadd13e830a0a40c8cb9ac3e
Till Rohrmann <trohrmann@apache.org>
2020-06-27 18:17:06 +0200

    [aoc_2019] Add tests for aoc 18 part 1

--- aoc_2019_18/src/lib.rs
+++ aoc_2019_18/src/lib.rs
@@ -1,4 +1,4 @@
-use aoc_common::GenericResult;
+use aoc_common::{GenericResult, GenericError};
 use std::convert::{TryInto, TryFrom};
 use core::fmt;
 use std::fmt::Formatter;
@@ -59,12 +59,6 @@ impl fmt::Display for MapElement {
     }
 }
 
-impl Into<char> for MapElement {
-    fn into(self) -> char {
-        unimplemented!()
-    }
-}
-
 impl Map {
     fn new(map: Vec<MapElement>, width: usize) -> Map {
         Map{
@@ -77,19 +71,126 @@ impl Map {
 pub fn read_map(filename: &str) -> GenericResult<Map> {
     let lines = aoc_common::read_raw_file_content(filename)?;
 
-    let width = lines[0].len();
+    read_map_from_lines(lines)
+}
 
+fn read_map_from_lines(lines: Vec<String>) -> GenericResult<Map> {
+    let width = lines[0].len();
     let result: Result<Vec<MapElement>, String> = lines.into_iter().flat_map(|line| {
         line.chars().map(|c| c.try_into()).collect::<Vec<Result<MapElement, String>>>()
     }).collect();
-
     Ok(Map::new(result?, width))
+}
+
+fn read_from_string(map: &str) -> GenericResult<Map> {
+    let lines: Vec<String> = map.split("\n").map(|l| l.to_string()).collect();
+
+    read_map_from_lines(lines)
+}
+
+struct Solver {
+
+}
+
+impl Solver {
+    fn new(map: &Map) -> Solver {
+        Solver{}
+    }
+
+    fn solve(&self) -> usize {
+        0
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_simple_map() {
+        let map = get_simple_map();
+        run_test(&map, 8);
+    }
+
+    #[test]
+    fn test_simple_map_2() {
+        let map = get_simple_map_2();
+        run_test(&map, 86);
+    }
+
+    #[test]
+    fn test_simple_map_3() {
+        let map = get_simple_map_3();
+        run_test(&map, 132);
+    }
+
+    #[test]
+    fn test_simple_map_4() {
+        let map = get_simple_map_4();
+        run_test(&map, 136);
+    }
+
+    #[test]
+    fn test_simple_map_5() {
+        let map = get_simple_map_5();
+        run_test(&map, 81);
+    }
+
+    fn run_test(map: &Map, expected_result: usize) {
+        let solver = Solver::new(map);
+
+        assert_eq!(solver.solve(), expected_result);
+    }
+
+    fn get_simple_map() -> Map {
+        let input = r"#########
+#b.A.@.a#
+#########";
+        read_from_string(input).unwrap()
+    }
+
+    fn get_simple_map_2() -> Map {
+        let input = r"########################
+#f.D.E.e.C.b.A.@.a.B.c.#
+######################.#
+#d.....................#
+########################";
+
+        read_from_string(input).unwrap()
+    }
+
+    fn get_simple_map_3() -> Map {
+        let input = r"########################
+#...............b.C.D.f#
+#.######################
+#.....@.a.B.c.d.A.e.F.g#
+########################";
+
+        read_from_string(input).unwrap()
+    }
+
+    fn get_simple_map_4() -> Map {
+        let input = r"#################
+#i.G..c...e..H.p#
+########.########
+#j.A..b...f..D.o#
+########@########
+#k.E..a...g..B.n#
+########.########
+#l.F..d...h..C.m#
+#################";
+
+        read_from_string(input).unwrap()
+    }
+
+    fn get_simple_map_5() -> Map {
+        let input = r"########################
+#@..............ac.GI.b#
+###d#e#f################
+###A#B#C################
+###g#h#i################
+########################";
+
+        read_from_string(input).unwrap()
     }
 }
