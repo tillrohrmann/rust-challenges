5d10dd8ca94bf36d0671c96e1add90fa181f9fd0
Till Rohrmann <trohrmann@apache.org>
2020-12-09 22:00:10 +0100

    [aoc_2020][day 5] Solve part 2

--- aoc_2020_5/src/bin/main.rs
+++ aoc_2020_5/src/bin/main.rs
@@ -7,7 +7,20 @@ fn main() {
         .collect::<Result<Vec<BoardingPass>, String>>()
         .unwrap();
 
-    let result = boarding_passes.iter().map(|boarding_pass| boarding_pass.seat_id()).max();
+    let mut seat_ids: Vec<usize> = boarding_passes.iter().map(|boarding_pass| boarding_pass.seat_id()).collect();
 
-    println!("Result {}", result.unwrap())
+    let result = seat_ids.iter().max().unwrap().to_owned();
+
+    seat_ids.sort();
+
+    let mut id = 0;
+
+    for i in 0..(seat_ids.len() - 1) {
+        if seat_ids[i + 1] - seat_ids[i] > 1 {
+            id = seat_ids[i] + 1;
+        }
+    }
+
+    println!("Result part 1 {}", result);
+    println!("Result part 2 {}", id);
 }
