3d222d52d98c048e01e0a567c9e3f8ef939a4d19
Till Rohrmann <trohrmann@apache.org>
2021-12-11 20:09:48 +0100

    [aoc_2021] Solve day 10 part 2

--- aoc_2021_10/src/lib.rs
+++ aoc_2021_10/src/lib.rs
@@ -2,14 +2,14 @@ use std::collections::HashSet;
 use aoc_common::GenericResult;
 
 enum ParseResult {
-    Ok,
+    Ok(Vec<char>),
     Error(char),
 }
 
 pub fn score_input(input: &Vec<String>) -> GenericResult<u32> {
     let result = input.iter().map(|line| score(line)).map(|result| {
         match result {
-            ParseResult::Ok => 0,
+            ParseResult::Ok(_) => 0,
             ParseResult::Error(error) => get_value(error),
         }
     }).sum();
@@ -59,7 +59,8 @@ fn score(line: &String) -> ParseResult {
         }
     }
 
-    ParseResult::Ok
+    stack.reverse();
+    ParseResult::Ok(stack)
 }
 
 fn get_value(ch: char) -> u32 {
@@ -72,12 +73,50 @@ fn get_value(ch: char) -> u32 {
     }
 }
 
+pub fn score_auto_completion(input: &Vec<String>) -> GenericResult<usize> {
+    let mut result: Vec<usize> = input.iter().map(|line| score(line)).flat_map(|result| {
+        match result {
+            ParseResult::Ok(completion) => Some(score_completion(&completion)),
+            ParseResult::Error(_) => None,
+        }
+    }).collect();
+
+    result.sort();
+
+    Ok(result[result.len()/2])
+}
+
+fn score_completion(completion: &Vec<char>) -> usize {
+    let mut result = 0;
+
+    for &ch in completion {
+        result = result * 5 + value(ch);
+    }
+
+    result
+}
+
+fn value(ch: char) -> usize {
+    match ch {
+        '(' => 1,
+        '[' => 2,
+        '{' => 3,
+        '<' => 4,
+        _ => 0,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn simple_test() {
+        let input = input();
+        assert_eq!(score_input(&input).unwrap(), 26397);
+    }
+
+    fn input() -> Vec<String> {
         let input: Vec<String> = "[({(<(())[]>[[{[]{<()<>>
 [(()[<>])]({[<{<<[]>>(
 {([(<{}[<>[]}>{[]{[(<()>
@@ -88,6 +127,12 @@ mod tests {
 [<(<(<(<{}))><([]([]()
 <{([([[(<>()){}]>(<<{{
 <{([{{}}[<[[[<>{}]]]>[]]".split('\n').map(|str| str.to_string()).collect();
-        assert_eq!(score_input(&input).unwrap(), 26397);
+        input
+    }
+
+    #[test]
+    fn test_auto_completion_scoring() {
+        let input = input();
+        assert_eq!(score_auto_completion(&input).unwrap(), 288957)
     }
 }
--- aoc_2021_10/src/main.rs
+++ aoc_2021_10/src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
     let content = aoc_common::read_raw_file_content("input.txt").unwrap();
 
+    solve_part_one(&content);
+    solve_part_two(&content);
+}
+
+fn solve_part_one(content: &Vec<String>) {
     let result = aoc_2021_10::score_input(&content).unwrap();
     println!("Part one: {}", result);
+}
+
+fn solve_part_two(content: &Vec<String>) {
+    let result = aoc_2021_10::score_auto_completion(&content).unwrap();
+    println!("Part two: {}", result);
 }
